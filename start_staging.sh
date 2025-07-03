84ff3b8c2091ee263d2cc47d1ac52d697ed3e025
Markus Sprunck <sprunck@muenchen.ihk.de>
2025-07-03 09:32:19 +0200

    CBC-984 Update Docker Setup for Testing
    - openethereum:alpine -> openethereum:latest
    - cbc-metrics:latest -> c4t-metrics:6.9.0
    - cleanup genKeys.sh
    - add docker pull images

--- start_staging.sh
+++ start_staging.sh
@@ -4,7 +4,7 @@ echo "##########################################################################
 echo "# 1. Remove running container and pull latest image"
 echo "###################################################################################"
 
-docker pull --platform linux/x86_64 ihkmuenchen/openethereum:latest
+docker pull --platform linux/x86_64 ihkmuenchen/openethereum:latest-rust-1.85
 docker pull --platform linux/x86_64 ihkmuenchen/c4t-metrics:6.9.0
 
 docker-compose down
